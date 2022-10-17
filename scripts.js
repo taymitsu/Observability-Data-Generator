00bcb5eef16ff7f22ffcbc9c0441740d76ecc47a
taymitsu <tmitsumori96@gmail.com>
2022-10-17 09:57:38 -0700

    Jeremy Status Bar manualy added

--- scripts.js
+++ scripts.js
@@ -25,3 +25,20 @@ for (let i = 0; i < data.length; i += 1) {
 	button.innerHTML = "Play"
 	newDiv.appendChild(button)
 }
+
+/////////////////////Jeremy Status Bar Functionality 
+$(document).ready(function () {
+  $('.dropdown-submenu a.test').on("click", function (e) {
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+  });
+});
+function changeImage(){
+  let displayImage = document.getElementById("image1")
+  if(displayImage.src.match("./icons/play.png")){
+      displayImage.src = "./icons/pause.png"
+  } else {
+      displayImage.src = "./icons/play.png"
+  }
+}
