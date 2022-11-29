ab7e62bb50cac19cd7fb30a509dbbb236b849540
alaJream <jeremy.gonzales@students.dominican.edu>
2022-11-28 21:09:36 -0800

    fixing onclick image for recents

--- scripts.js
+++ scripts.js
@@ -95,18 +95,18 @@ button2.addEventListener('click', (evt) => {
 
 
 
-// $(document).ready(function () {
-//   $('.dropdown-submenu a.test').on("click", function (e) {
-//       $(this).next('ul').toggle();
-//       e.stopPropagation();
-//       e.preventDefault();
-//   });
-// });
-// function changeImage(){
-//   let displayImage = document.getElementById("image1")
-//   if(displayImage.src.match("./icons/play.png")){
-//       displayImage.src = "./icons/pause.png"
-//   } else {
-//       displayImage.src = "./icons/play.png"
-//   }
-// }
+$(document).ready(function () {
+  $('.dropdown-submenu a.test').on("click", function (e) {
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+  });
+});
+function changeImage(){
+  let displayImage = document.getElementById("image1")
+  if(displayImage.src.match("./icons/play.png")){
+      displayImage.src = "./icons/pause.png"
+  } else {
+      displayImage.src = "./icons/play.png"
+  }
+}
